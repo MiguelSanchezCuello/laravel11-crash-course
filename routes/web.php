37e1b4eca3c95ce416ce58d941c210cdf62ba3f1
MIGUEL SANCHEZ <miguelsc10@hotmail.com>
2024-07-20 22:34:54 -0400

    follow up with the project

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,6 @@
 <?php
 
-use App\Http\Controllers\WelcomeController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\WelcomeController;
 
-Route::get('/', [WelcomeController::class, 'welcome'])->name();
+Route::get('/', [WelcomeController::class, 'welcome'])->name('welcome');
